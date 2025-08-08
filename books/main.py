d17c2bf8a1e11cf4cdb541947acd4afdff91fd91
Tran Vinh Thuan <thuan.tran@bstarsolutions.com>
2025-08-08 16:01:46 +0700

    save book

--- books/main.py
+++ books/main.py
@@ -3,44 +3,29 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from typing import Optional, List
 from enum import IntEnum
+from uuid import UUID
 
 app = FastAPI()
 
-class Book:
-    id: int
-    title: str
-    author:str
-    description: str
-    rating: int
-    def __init__(self,id, title, author, description, rating):
-        self.title = title
-        self.author = author
-        self.description = description
-        self.rating = rating
-        self.id = id
-
-class BookBase(BaseModel):
-    # id: int = Field(..., description='Unique identifier of the book')
+class Book(BaseModel):
+    id: int = Field(..., description='Unique identifier of the book')
     title: str = Field(..., min_length=1, max_length=512, description='Title of the book')
     author: str = Field(..., min_length=1, max_length=512, description='Author of the book')
     description: str = Field(..., min_length=1, max_length=1000, description='Description of the book')
-    rating: int = Field(gt=0, lt=5,description='The rating of the book')
+    rating: int = Field(gt=0, lt=6, description='The rating of the book')
 
-class BookRes(BookBase):
-    id: int = Field(..., description='Unique identifier of book')
 
-class BookCreate(BookBase):
+class BookCreate(Book):
     pass
 
 class BookUpdate(BaseModel):
-    title: Optional[str] = Field(None, min_length=1, max_length=512, description='Title of the book')
-    author: Optional[str] = Field(..., min_length=1, max_length=512, description='Author of the book')
+    title: Optional[str] = Field(None, description='Title of the book')
+    author: Optional[str] = Field(..., description='Author of the book')
     description: Optional[str] = Field(..., min_length=1, max_length=1000, description='Description of the book')
-    rating: Optional[int] = Field(gt=0, lt=5,description='The rating of the book')
+    rating: Optional[int] = Field(...,gt=0, lt=6, description='The rating of the book')
 
 
 all_books = [
-    Book(id=1, title='Todo1', author='Todo1', description='Todo1 description', rating=1),
 ]
 @app.get("/")
 def index():
@@ -53,19 +38,25 @@ def get_all_books(limit: int  = None):
     else:
         return all_books[:limit]
 
-@app.get("/books/{id}", response_model = BookRes)
+@app.get("/books/{id}", response_model = Book)
 def get_book(id:int):
     for book in all_books:
         if book.id == id:
             return book
     raise HTTPException(status_code=404, detail="The book not found")
 
-@app.post('/books', response_model=BookRes)
+@app.post('/books', response_model=Book)
 def create_book(book:BookCreate):
     if not book:
         raise HTTPException(status_code=404, detail="The book not empty")
-    new_id = max(bk.id for bk in all_books) + 1
+    all_books.append(book)
+    return book
 
-    new_book = Book(**book.model_dump(), id=new_id, )
-    all_books.append(new_book)
-    return new_book
+@app.put("/books/{id}")
+def update_book(id:int, updated_book:BookUpdate):
+    for i, book in enumerate(all_books):
+        if book.id == id:
+            _book = Book(**updated_book.model_dump(), id=id )
+            all_books[i] = _book
+            return _book
+    raise HTTPException(status_code=404, detail="The book not found")
