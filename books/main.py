e400c6e2b682c2ca1aad6ad26740b8e73e717d8b
Tran Vinh Thuan <thuan.tran@bstarsolutions.com>
2025-08-08 17:37:25 +0700

    handle update and delete books project

--- books/main.py
+++ books/main.py
@@ -20,9 +20,9 @@ class BookCreate(Book):
 
 class BookUpdate(BaseModel):
     title: Optional[str] = Field(None, description='Title of the book')
-    author: Optional[str] = Field(..., description='Author of the book')
-    description: Optional[str] = Field(..., min_length=1, max_length=1000, description='Description of the book')
-    rating: Optional[int] = Field(...,gt=0, lt=6, description='The rating of the book')
+    author: Optional[str] = Field(None, description='Author of the book')
+    description: Optional[str] = Field(None, min_length=1, max_length=1000, description='Description of the book')
+    rating: Optional[int] = Field(None,gt=0, lt=6, description='The rating of the book')
 
 
 all_books = [
@@ -43,12 +43,12 @@ def get_book(id:int):
     for book in all_books:
         if book.id == id:
             return book
-    raise HTTPException(status_code=404, detail="The book not found")
+    raise HTTPException(status_code=404, detail="The book is not found")
 
 @app.post('/books', response_model=Book)
 def create_book(book:BookCreate):
     if not book:
-        raise HTTPException(status_code=404, detail="The book not empty")
+        raise HTTPException(status_code=404, detail="The book is not empty")
     all_books.append(book)
     return book
 
@@ -56,7 +56,16 @@ def create_book(book:BookCreate):
 def update_book(id:int, updated_book:BookUpdate):
     for i, book in enumerate(all_books):
         if book.id == id:
-            _book = Book(**updated_book.model_dump(), id=id )
-            all_books[i] = _book
-            return _book
-    raise HTTPException(status_code=404, detail="The book not found")
+            updated = book.model_copy(update=updated_book.model_dump(exclude_unset=True))
+            all_books[i] = updated
+            return updated
+    raise HTTPException(status_code=404, detail="The book is not found")
+
+
+@app.delete("/books/{id}")
+def delete_book(id:int):
+    for i, book in enumerate(all_books):
+        if book.id == id:
+            all_books.pop(i)
+            return book
+    raise HTTPException(status_code=404, detail="The book is not found")
